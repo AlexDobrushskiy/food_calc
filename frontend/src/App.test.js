a1657ea5e594f0f19694daa2b81e37443585836b
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2019-02-03 20:09:30 +0300

    Some tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {saveAPIToken, setCurrentPage} from "./actions";
+import * as actionTypes from "./actionTypes";
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('actions', () => {
+    it('should save api token', () => {
+        const expectedAction = {
+            type: actionTypes.SAVE_API_TOKEN,
+            token: 'sometokenvaluehere'
+        };
+        expect(saveAPIToken('sometokenvaluehere')).toEqual(expectedAction);
+    });
+    it('should set current page number', () => {
+        const expectedAction = {
+            type: actionTypes.SET_CURRENT_PAGE,
+            number: 99
+        };
+        expect(setCurrentPage(99)).toEqual(expectedAction);
+    });
 });
